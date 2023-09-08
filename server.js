56d4ae7b6c3ef1aaa9a6602bbc14c8bf7332537d
Prayasu Sidha Satapathy <satapathyprayasu@gmail.com>
2023-09-08 16:02:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,21 @@ app.use(bodyParser.json())
 // "data": [“M”,”1”,”334”,”4”,”B”]
 
 app.post("/bfhl", (req, res) => {
-  const data = req.body.data
+  const data = req.body.data;
   console.log(data)
-  const numbers = []
-  const alphabets = []
-  let highest_alphabet = ""
 
-  for (const element of data) {
-    if (element.match(/^[0-9]$/)) {
-      numbers.push(element)
-    } else if (element.match(/^[a-z]$/i)) {
-      alphabets.push(element)
-    }
+  
+    const alphabets = data.filter(item => typeof item === 'string' && item.length === 1);
+    // const numbers = data.filter(item => typeof item === 'number' && item.length === 1);
 
-    if (alphabets.length > 0 && element > highest_alphabet) {
-      highest_alphabet = element
-    }
-  }
-  res.json({
+    const numbers = data.filter(item => !isNaN(item));
+
+    console.log(data);
+
+    const highest_alphabet = alphabets.length > 0
+    ? alphabets.reduce((a, b) => (a > b ? a : b)) : null;
+
+res.json({
     is_success: true,
     user_id: "prayasu_sidha_satapathy_18_03_2003",
     email: "satapathyprayasu@gmail.com",
@@ -42,3 +39,6 @@ app.get("/bfhl", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is listening on port 3000")
 })
+  
+
+
